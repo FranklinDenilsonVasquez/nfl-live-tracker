f14f692cbab7936b4fae7c8e60aa5f6468fa0d2f
Franklin Vasquez <franklinvasquez@Janmarcoss-MacBook-Pro.local>
2025-09-24 15:41:27 -0500

    Implemented skeleton TeamList component.

---
+++ src/frontend/src/components/TeamList.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import "./TeamList.css";
+
+function TeamList(){
+
+    const [teamSelected, setTeamSelected] = useState("");
+
+    const onClickTeamSelect = (team) => {
+        setTeamSelected(team.target.innerText)
+    }
+
+    return (
+        <div>
+            <main className="main-container">
+        <div className="team-info-div-container">
+          <div className="team-info-group-container">AFC
+            <ul>
+              <li className="team-info-division-list" onClick={onClickTeamSelect}>Team 1</li>
+              <li className="team-info-division-list" onClick={onClickTeamSelect}>Team 2</li>
+            </ul>
+          </div>
+          <div className="team-info-group-container">NFC
+            <ul>
+              <li className="team-info-division-list" onClick={onClickTeamSelect}>Team 1</li>
+              <li className="team-info-division-list" onClick={onClickTeamSelect}>Team 2</li>
+            </ul>
+          </div>
+        </div>
+        <div className="schedule-div-container"> 
+            {teamSelected ? (<p>Selected team {teamSelected}</p>):(<p>Select a team for their schedule</p>)}
+        </div>
+        <div className="team-info-div-container">Standings</div>
+      </main>
+        </div>
+    )
+}
+
+export default TeamList
--- src/frontend/src/pages/MainPage.js
+++ src/frontend/src/pages/MainPage.js
@@ -2,29 +2,14 @@ import React from "react";
 import "./MainPage.css";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import TeamList from "../components/TeamList";
+
 
 export default function HomePage() {
   return (
     <div className="app-container">
       <Header />
-      <main className="main-container">
-        <div className="team-info-div-container">Teams
-          <div className="team-info-group-container">AFC
-            <ul>
-              <li className="team-info-division-list">Team 1</li>
-              <li className="team-info-division-list">Team 2</li>
-            </ul>
-          </div>
-          <div className="team-info-group-container">NFC
-            <ul>
-              <li className="team-info-division-list">Team 1</li>
-              <li className="team-info-division-list">Team 2</li>
-            </ul>
-          </div>
-        </div>
-        <div className="schedule-div-container"> Schedlue Info</div>
-        <div className="team-info-div-container">Standings</div>
-      </main>
+      <TeamList/>
       <Footer />
     </div>
   );
