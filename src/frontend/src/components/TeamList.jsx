4da693a7bc284ddc573e5589cb7fc29712c3dce1
Franklin Vasquez <franklinvasquez@Janmarcoss-MacBook-Pro.local>
2025-09-24 16:49:21 -0500

    Implemented dynamic Standings in TeamList component.

--- src/frontend/src/components/TeamList.jsx
+++ src/frontend/src/components/TeamList.jsx
@@ -12,25 +12,29 @@ function TeamList(){
     return (
         <div>
             <main className="main-container">
-        <div className="team-info-div-container">
-          <div className="team-info-group-container">AFC
-            <ul>
-              <li className="team-info-division-list" onClick={onClickTeamSelect}>Team 1</li>
-              <li className="team-info-division-list" onClick={onClickTeamSelect}>Team 2</li>
-            </ul>
-          </div>
-          <div className="team-info-group-container">NFC
-            <ul>
-              <li className="team-info-division-list" onClick={onClickTeamSelect}>Team 1</li>
-              <li className="team-info-division-list" onClick={onClickTeamSelect}>Team 2</li>
-            </ul>
-          </div>
-        </div>
-        <div className="schedule-div-container"> 
-            {teamSelected ? (<p>Selected team {teamSelected}</p>):(<p>Select a team for their schedule</p>)}
-        </div>
-        <div className="team-info-div-container">Standings</div>
-      </main>
+                <div className="team-info-div-container">
+                <div className="team-info-group-container">AFC
+                    <ul>
+                    <li className="team-info-division-list" onClick={onClickTeamSelect}>Team 1</li>
+                    <li className="team-info-division-list" onClick={onClickTeamSelect}>Team 2</li>
+                    </ul>
+                </div>
+                <div className="team-info-group-container">NFC
+                    <ul>
+                    <li className="team-info-division-list" onClick={onClickTeamSelect}>Team 1</li>
+                    <li className="team-info-division-list" onClick={onClickTeamSelect}>Team 2</li>
+                    </ul>
+                </div>
+                </div>
+                <div className="schedule-div-container"> 
+                    {teamSelected ? (<p>Selected team {teamSelected}</p>):(<p>Select a team for their schedule</p>)}
+                </div>
+                <div className="team-info-div-container">Standings
+                    {
+                        teamSelected ? (<p>{teamSelected} current standings</p>):(<p>Overall standings</p>)
+                    }
+                </div>
+            </main>
         </div>
     )
 }
