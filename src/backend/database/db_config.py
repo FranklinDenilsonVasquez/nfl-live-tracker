dd4e8600432f7f447e6ced650e8e66396f01655e
Franklin D. Vasquez <franklindenilsonvasquez@gmail.com>
2026-07-13 17:53:07 -0500

    Connect FastAPI to Railway Postgres

--- src/backend/database/db_config.py
+++ src/backend/database/db_config.py
@@ -7,9 +7,9 @@ load_dotenv()
 
 def get_db_config():
     return {
-        'host': os.getenv('DB_HOST'),
-        'port': os.getenv('DB_PORT', 5432),
-        'user': os.getenv('DB_USER'),
-        'password': os.getenv('DB_PASSWORD'),
-        'database': os.getenv('DB_NAME')
+        'host': os.getenv('PGHOST'),
+        'port': os.getenv('PGPORT', 5432),
+        'user': os.getenv('PGUSER'),
+        'password': os.getenv('PGPASSWORD'),
+        'database': os.getenv('PGDATABASE')
     }
