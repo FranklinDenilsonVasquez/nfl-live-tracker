18cba22c150447020ca0a8f348c1270e4afdc8a3
Franklin D. Vasquez <franklindenilsonvasquez@gmail.com>
2026-09-03 02:51:31 -0500

    Updated .env file to include CORS allowed origins

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,25 +1,23 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
-
+import os
 from src.backend.api.routes.players import router as player_router
 from src.backend.api.routes.teams import router as team_router
 from src.backend.api.routes.games import router as game_router
 from src.backend.api.routes.standing import router as standing_router 
+
+origins_env = os.environ.get("ALLOWED_ORIGINS",  "http://localhost:3000,http://127.0.0.1:3000")
+origins = [o.strip() for o in origins_env.split(",")]
+
 app = FastAPI(
     title="NFL Stats API",
     description="API for fetching NFL teams and players",
     version="1.0.0"
 )
 
-origins = [
-    "http://localhost:3000",  # React dev server
-    "http://127.0.0.1:3000",
-    "https://nfl-live-tracker-sxde-mu.vercel.app",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_methods=["*"],
     allow_headers=["*"],
 )
