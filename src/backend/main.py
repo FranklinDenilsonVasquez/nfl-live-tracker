fd12a120ca9ed5091556687be00d177665587629
Franklin D. Vasquez <franklindenilsonvasquez@gmail.com>
2026-09-03 02:13:21 -0500

    Bug: CORS error

--- src/backend/main.py
+++ src/backend/main.py
@@ -14,6 +14,7 @@ app = FastAPI(
 origins = [
     "http://localhost:3000",  # React dev server
     "http://127.0.0.1:3000",
+    "https://nfl-live-tracker-sxde-mu.vercel.app",
 ]
 
 app.add_middleware(
